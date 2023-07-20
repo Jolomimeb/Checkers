7003e1bb33228e6c03ef6c305f74a2c6b415c18f
Jolomimeb <111466708+Jolomimeb@users.noreply.github.com>
2023-07-19 20:22:45 -0400

    mytest.cpp

--- mytest.cpp
+++ mytest.cpp
@@ -1,20 +1,3 @@
-/*****************************************
- ** File:    mytest.cpp
- ** Project: CMSC 341 Project 0, Spring 2023
- ** Author:  Oritsejolomisan Mebaghanje
- ** Date:    02/11/23
- ** E-mail:  xz94254@umbc.edu
- **
- ** This file contains the program for mytest.cpp.
- **
- **
- **
- **
- **
- **
- **
- ***********************************************/
-
 #include <iostream>
 #include "checkers.h"
 
